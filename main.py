499568d76133d77621f414835cfb602ce99ea119
Job Preedanan <job.preedanan@gmail.com>
2021-09-18 23:08:45 +0900

    fix stage 1

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import numpy as np
 import cv2
 import os
 from unet_model import UNet
-from utils import create_KUB_partitions, combine_KUB_partitions, normalize, denormalize, remove_small_blobs
+from utils import create_KUB_partitions, combine_KUB_partitions, normalize, \
+    denormalize, remove_small_blobs, make_square, heatmap_generate
 import matplotlib.pyplot as plt
 
 
@@ -13,14 +14,26 @@ cfg = {'image_size': 256,
        'cascade_unet': True}
 
 
-def load_images_from_folder(folder_name):
+def load_images_from_folder(folder_name, image_size, resize_method=None, image_type='gray'):
     images_list = os.listdir(folder_name)
-    images = np.zeros((len(images_list), cfg['image_size'], cfg['image_size'], 1))
+    images = np.zeros((len(images_list), image_size, image_size, 1), np.float32)
+
     for i, image_name in enumerate(images_list):
+
+        # read image
         image = cv2.imread(folder_name + os.sep + image_name, cv2.IMREAD_GRAYSCALE)
-        image = cv2.resize(image, (cfg['image_size'], cfg['image_size']))
+
+        # resize
+        if resize_method == 'fill':
+            image = make_square(image)
+        image = cv2.resize(image, (image_size, image_size))
+
         image = image[:, :, np.newaxis]
-        images[i] = normalize(image)
+
+        if image_type == 'bw':
+            images[i] = normalize(image, img_type='bw')
+        elif image_type == 'gray':
+            images[i] = normalize(image, img_type='gray')
 
     return images
 
@@ -28,21 +41,36 @@ def load_images_from_folder(folder_name):
 # 1st-stage : KUB map generation
 def kub_map_generation(full_images):
 
-    # U-Net define
+    # preprocessing (256 x 256)
+    def resize_tensor(tensor):
+        new_tensor = np.zeros((tensor.shape[0], cfg['image_size'], cfg['image_size'], 1), np.float32)
+        for i in range(tensor.shape[0]):
+            image = cv2.resize(np.squeeze(tensor[i]), (cfg['image_size'], cfg['image_size']))
+            new_tensor[i] = image[:, :, np.newaxis]
+        return new_tensor
+
+    # define U-Net
     network = UNet(input_channel_count=1, output_channel_count=1, first_layer_filter_count=32)
     model = network.get_model()
     model.load_weights('unet_weights_stage_1.hdf5')
 
-    # prediction
-    y_preds = model.predict(full_images, batch_size=16)
+    # resize to 256 x 256
 
-    kub_map = np.zeros_like(y_preds)
+    full_images_low_res = resize_tensor(full_images)
+
+    # prediction
+    y_preds = model.predict(full_images_low_res, batch_size=16)
+
+    kub_map = np.zeros_like(full_images)
+
     for i, y_pred in enumerate(y_preds):
+
+        hm = heatmap_generate(full_images_low_res[i], y_pred)
         y_pred = cv2.resize(y_pred, (full_images[i].shape[1], full_images[i].shape[0]))
         y_pred = denormalize(y_pred, img_type='gt')
         y_pred = np.array(y_pred, dtype=np.uint8)
 
-        # post-processing (binary + remove small contours)
+        # post-processing (binary + remove small cc)
         ret, y_pred_th = cv2.threshold(y_pred, 127, 255, cv2.THRESH_BINARY)
         y_pred_th = remove_small_blobs(y_pred_th)
 
@@ -51,29 +79,38 @@ def kub_map_generation(full_images):
     return kub_map
 
 
-def stones_segmentation(full_images, cascade=True):
+def stones_segmentation(full_images, cascade=True, method=0):
     import cv2
 
     if cascade:
 
         # 1st stage: KUB generation
-        kub_maps = kub_map_generation(full_images)
-        images = np.zeros((len(full_images)*3, cfg['image_size'], cfg['image_size'], 1))
-        f, subfig = plt.subplots(len(full_images), 7)
+        if method == 0:
+            kub_maps = kub_map_generation(full_images)
+        else:
+            kub_maps = load_images_from_folder('kub_map',
+                                               image_size=cfg['full_image_size'],
+                                               image_type='bw')
+
+        images = np.zeros((len(full_images)*3, cfg['image_size'], cfg['image_size'], 1), np.float32)
+        # f, subfig = plt.subplots(len(full_images), 8)
         for i, full_image in enumerate(full_images):
+
+            ret, kub_map = cv2.threshold(kub_maps[i], 0, 255, cv2.THRESH_BINARY)
 
             # create 3 partitions
             l_partition, r_partition, b_partition = create_KUB_partitions(np.squeeze(full_image),
-                                                                          np.squeeze(kub_maps[i]))
+                                                                          np.squeeze(kub_map))
 
             l_partition = cv2.resize(l_partition, (cfg['image_size'], cfg['image_size']))
             r_partition = cv2.resize(r_partition, (cfg['image_size'], cfg['image_size']))
             b_partition = cv2.resize(b_partition, (cfg['image_size'], cfg['image_size']))
 
-            subfig[i, 0].imshow(denormalize(np.squeeze(full_image)), cmap='gray', vmin=0, vmax=255)
-            subfig[i, 1].imshow(denormalize(l_partition), cmap='gray', vmin=0, vmax=255)
-            subfig[i, 3].imshow(denormalize(r_partition), cmap='gray', vmin=0, vmax=255)
-            subfig[i, 5].imshow(denormalize(b_partition), cmap='gray', vmin=0, vmax=255)
+            # subfig[i, 0].imshow(denormalize(np.squeeze(full_image)), cmap='gray', vmin=0, vmax=255)
+            # subfig[i, 1].imshow(denormalize(kub_map), cmap='gray', vmin=0, vmax=255)
+            # subfig[i, 2].imshow(denormalize(l_partition), cmap='gray', vmin=0, vmax=255)
+            # subfig[i, 4].imshow(denormalize(r_partition), cmap='gray', vmin=0, vmax=255)
+            # subfig[i, 6].imshow(denormalize(b_partition), cmap='gray', vmin=0, vmax=255)
 
             images[3 * i] = l_partition[:, :, np.newaxis]
             images[3 * i + 1] = r_partition[:, :, np.newaxis]
@@ -85,7 +122,7 @@ def stones_segmentation(full_images, cascade=True):
         model.load_weights('unet_weights_stage_2.hdf5')
 
         # predict
-        y_pred = model.predict(images)
+        y_pred = model.predict(images, batch_size=16)
 
         # combine partitioned y_pred into full y_pred
         y_pred = np.squeeze(y_pred)
@@ -94,15 +131,21 @@ def stones_segmentation(full_images, cascade=True):
         for i in range(0, len(y_pred), 3):
             partitions = y_pred[i:i + 3]
 
-            subfig[n, 2].imshow(denormalize(partitions[0]), cmap='gray', vmin=0, vmax=255)
-            subfig[n, 4].imshow(denormalize(partitions[1]), cmap='gray', vmin=0, vmax=255)
-            subfig[n, 6].imshow(denormalize(partitions[2]), cmap='gray', vmin=0, vmax=255)
+            # subfig[n, 3].imshow(partitions[0]*255, cmap='gray', vmin=0, vmax=255)
+            # subfig[n, 5].imshow(partitions[1]*255, cmap='gray', vmin=0, vmax=255)
+            # subfig[n, 7].imshow(partitions[2]*255, cmap='gray', vmin=0, vmax=255)
 
-            full_kub_map = cv2.resize(kub_maps[n], (cfg['full_image_size'], cfg['full_image_size']))
-            full_y_pred[n] = combine_KUB_partitions(partitions[0], partitions[1], partitions[2], full_kub_map)
+            full_kub_map = cv2.resize(kub_maps[n], (full_image.shape[1], full_image.shape[0]))
+            ret, full_kub_map = cv2.threshold(full_kub_map, 0, 255, cv2.THRESH_BINARY)
+            combined = combine_KUB_partitions(partitions[0], partitions[1], partitions[2], full_kub_map)
+
+            # post-processing (binary + remove small cc)
+            combined = denormalize(combined, img_type='gt')
+            combined = np.array(combined, dtype=np.uint8)
+            ret, combined = cv2.threshold(combined, 127, 255, cv2.THRESH_BINARY)
+            full_y_pred[n] = remove_small_blobs(combined, min_size=10)
+
             n += 1
-
-        plt.show()
 
         return full_y_pred, kub_maps
 
@@ -123,34 +166,41 @@ def stones_segmentation(full_images, cascade=True):
 if __name__ == '__main__':
     print(tf.__version__)
 
-    test_image = load_images_from_folder('test_images')
-    test_gt = load_images_from_folder('test_gt')
-    results, kub_maps = stones_segmentation(test_image, cascade=True)
+    # baseline method
+    test_image = load_images_from_folder('test_images', image_size=cfg['image_size'], resize_method='fill')
     result_baseline = stones_segmentation(test_image, cascade=False)
-    print(results.shape)
 
-    f, subfig = plt.subplots(results.shape[0], 5)
-    for i, result in enumerate(results):
-        subfig[i, 0].imshow(denormalize(test_image[i]), cmap='gray', vmin=0, vmax=255)
-        subfig[i, 0].axis('off')
-        subfig[i, 0].title.set_text('input')
-
-        subfig[i, 1].imshow(kub_maps[i], cmap='gray', vmin=0, vmax=255)
-        subfig[i, 1].axis('off')
-        subfig[i, 1].title.set_text('stone location map')
-
-        subfig[i, 2].imshow(denormalize(result_baseline[i], img_type='bw'), cmap='gray', vmin=0, vmax=255)
-        subfig[i, 2].axis('off')
-        subfig[i, 2].title.set_text('baseline')
-
-        subfig[i, 3].imshow(denormalize(result, img_type='bw'), cmap='gray', vmin=0, vmax=255)
-        subfig[i, 3].axis('off')
-        subfig[i, 3].title.set_text('casecade unet')
-
-        subfig[i, 4].imshow(denormalize(test_gt[i], img_type='bw'), cmap='gray', vmin=0, vmax=255)
-        subfig[i, 4].axis('off')
-        subfig[i, 4].title.set_text('gt')
+    # proposed method
+    test_image = load_images_from_folder('test_images', image_size=cfg['full_image_size'], resize_method='fill')
+    results, kub_maps = stones_segmentation(test_image, cascade=True)
     plt.show()
+
+    test_gt = load_images_from_folder('test_gt', image_size=cfg['full_image_size'], resize_method='fill')
+
+    # f, subfig = plt.subplots(results.shape[0], 5)
+    for i, result in enumerate(results):
+        f, subfig = plt.subplots(1, 5)
+
+        subfig[0].imshow(denormalize(test_image[i]), cmap='gray', vmin=0, vmax=255)
+        subfig[0].axis('off')
+        subfig[0].title.set_text('input')
+
+        subfig[1].imshow(kub_maps[i], cmap='gray', vmin=0, vmax=255)
+        subfig[1].axis('off')
+        subfig[1].title.set_text('stone location map')
+
+        subfig[2].imshow(denormalize(result_baseline[i], img_type='bw'), cmap='gray', vmin=0, vmax=255)
+        subfig[2].axis('off')
+        subfig[2].title.set_text('baseline')
+
+        subfig[3].imshow(denormalize(result, img_type='bw'), cmap='gray', vmin=0, vmax=255)
+        subfig[3].axis('off')
+        subfig[3].title.set_text('cascade unet')
+
+        subfig[4].imshow(denormalize(test_gt[i], img_type='bw'), cmap='gray', vmin=0, vmax=255)
+        subfig[4].axis('off')
+        subfig[4].title.set_text('gt')
+        plt.show()
 
 
 
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 
 # Normalize image
@@ -21,13 +22,14 @@ def denormalize(image, img_type='gray'):
     return image
 
 
-def remove_small_blobs(bw_image, min_size=1000):
+def remove_small_blobs(bw_image, min_size=3000):
+
     # find all your connected components (white blobs in your image)
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(bw_image, connectivity=8)
     sizes = stats[1:, -1]
     nb_components = nb_components - 1
 
-    image = np.zeros((output.shape))
+    image = np.zeros((bw_image.shape))
     # for every component in the image, you keep it only if it's above min_size
     for i in range(0, nb_components):
         if sizes[i] >= min_size:
@@ -36,24 +38,27 @@ def remove_small_blobs(bw_image, min_size=1000):
     return image
 
 
-def find_KUB_bounding_box(full_KUB_map, border_size=30):
+def find_KUB_bounding_box(full_KUB_map, border_size=0.1):
 
     # KUB map preprocessing
     full_KUB_map = np.array(full_KUB_map, dtype=np.uint8)
-    _, full_KUB_map = cv2.threshold(full_KUB_map, 0, 255, cv2.THRESH_BINARY)
-    kernel = np.ones((3, 3), np.uint8)
-    full_KUB_map = cv2.dilate(full_KUB_map, kernel, iterations=1)
+    # _, full_KUB_map = cv2.threshold(full_KUB_map, 0, 255, cv2.THRESH_BINARY)
+    kernel = np.ones((5, 5), np.uint8)
+    full_KUB_map = cv2.dilate(full_KUB_map, kernel, iterations=2)
 
     # find contour
     cnt_tmp = cv2.findContours(full_KUB_map, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contour = cnt_tmp[0] if len(cnt_tmp) == 2 else cnt_tmp[1]
     x, y, w, h = cv2.boundingRect(contour[0])    # parameters for L and R partitions
 
+    border_w = 30 #int(border_size * w)
+    border_h = 30 #int(border_size * h)
+
     # expand bb
-    x_top = max(0, x - border_size)
-    w_top = min(w + 2*border_size, full_KUB_map.shape[1] - x_top)
-    y_top = max(0, y - border_size)
-    h_top = round(h/2) + 2*border_size
+    x_top = max(0, x - border_w)
+    w_top = min(w + 2*border_w, full_KUB_map.shape[1] - x_top)
+    y_top = max(0, y - border_h)
+    h_top = round(h/2) + 2*border_h
 
     # find bladder partition
     low_img_map = full_KUB_map.copy()
@@ -62,12 +67,13 @@ def find_KUB_bounding_box(full_KUB_map, border_size=30):
     # find contour
     cnt_tmp = cv2.findContours(low_img_map, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     low_contour = cnt_tmp[0] if len(cnt_tmp) == 2 else cnt_tmp[1]
-    x_low, y_low, w_low, h_low = cv2.boundingRect(low_contour[0])  # parameters for B partition
+    x_low, y_low, w_low, h_low = cv2.boundingRect(low_contour[0])  # parameters for B partitio
+
     # expand bb
-    y_low = y_low - border_size
-    x_low = x_low - border_size
-    w_low = w_low + 2 * border_size
-    h_low = min(h_low + 2*border_size, full_KUB_map.shape[0] - y_low)
+    x_low = x_low - border_w
+    w_low = w_low + 2 * border_w
+    y_low = y_low - border_h
+    h_low = min(h_low + 2*border_h, full_KUB_map.shape[0] - y_low)
 
     # # display testing
     # display_img = np.zeros([full_KUB_map.shape[0], full_KUB_map.shape[1], 3])
@@ -135,3 +141,27 @@ def create_KUB_partitions_old(full_image):
     return left_partition, right_partition, bladder_partition
 
 
+def make_square(img):
+    s_size = max(img.shape[0:2])
+    new_image = np.zeros((s_size, s_size), np.float32)
+
+    # Getting the centering position
+    ax, ay = (s_size - img.shape[1]) // 2, (s_size - img.shape[0]) // 2
+
+    # Pasting the 'image' in a centering position
+    new_image[ay:img.shape[0] + ay, ax:ax + img.shape[1]] = img
+
+    return new_image
+
+
+def heatmap_generate(org_image, predicted):
+
+    heatmap = cv2.resize(predicted, (org_image.shape[1], org_image.shape[0]))
+    heatmap = np.maximum(heatmap, 0)
+    heatmap /= np.max(heatmap)
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    hif = .8
+    superimposed_img = heatmap * hif + org_image
+
+    return superimposed_img
